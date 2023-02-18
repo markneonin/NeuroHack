38eb862bf2927cadc50c8eeac60ae0e0c20ef818
markneonin <oninivan@gmail.com>
2023-02-19 00:53:06 +0300

    updated

--- statistic_consumer/app/statistic_consumer.py
+++ statistic_consumer/app/statistic_consumer.py
@@ -9,8 +9,6 @@ import motor.motor_asyncio
 from aiokafka import AIOKafkaConsumer
 from dotenv import load_dotenv
 
-from signals_mapping import signals_mapping
-
 dotenv_path = Path('./constants/.env')
 load_dotenv(dotenv_path=dotenv_path)
 
@@ -26,34 +24,73 @@ SASL_SSL = os.getenv('KAFKA_SASL_SSL')
 
 SSL_CONTEXT = ssl.create_default_context(cafile='./constants/CA.pem')
 
-client = motor.motor_asyncio.AsyncIOMotorClient(STATISTIC_DB, serverSelectionTimeoutMS=5000)
+client = motor.motor_asyncio.AsyncIOMotorClient(STATISTIC_DB)
 db = client.statistic
 
-offset = '32958'
+attribute_collection = {
+    'bearings_big': 'bearing_big',
+    'bearings_small': 'bearing_small',
+    'cooler': 'cooler',
+    'gas_manifold': 'gas_manifold',
+    'valve_position': 'valve_position',
+    'main_drive': 'main_drive',
+    'oil_system': 'oil_system',
+    'exgauster_work': 'exgauster_work',
+}
+
+offset = 'earliest'
 
 
-def parse_signals(msg):
-    timestamp = datetime.datetime.fromisoformat(msg.pop('moment'))
-    signals = [
-        {
-            'key': code,
-            'exgauster': signals_mapping.get(code).get('exgauster'),
-            'moment': timestamp,
-            'value': value,
-        }
-        for code, value in msg.items()
-    ]
-    return signals
+def parse_component(component, msg):
+    entity = {
+        'component_id': component['_id'],
+    }
+
+    for field, code in component['fields_map'].items():
+        entity[field] = msg.get(code)
+
+    return entity
 
 
-async def write_to_mongo(signals):
-    await db.signal.insert_many(signals)
+def parse_message(msg, exgausters):
+    moment = datetime.datetime.fromisoformat(msg.pop('moment'))
+    to_write = {}
+
+    for exg in exgausters:
+        for attr, collection in attribute_collection.items():
+
+            component = exg[attr]
+            if isinstance(component, list):
+
+                for sub_component in component:
+                    entity = parse_component(sub_component, msg)
+                    entity.update(moment=moment)
+                    to_write.setdefault(collection, []).append(entity)
+
+            else:
+                entity = parse_component(component, msg)
+                entity.update(moment=moment)
+                to_write.setdefault(collection, []).append(entity)
+
+    return to_write
 
 
-async def handle_message(msg: bytes):
+async def _insert_into_collection(collection, entities):
+    await db[collection].insert_many(entities)
+
+
+async def write_to_mongo(to_write):
+    await asyncio.gather(*(
+        asyncio.create_task(_insert_into_collection(collection, entities))
+        for collection, entities in to_write.items()
+    ))
+
+
+async def handle_message(msg: bytes, exgausters):
     msg = json.loads(msg.decode())
-    signals = parse_signals(msg)
-    # await write_to_mongo(signals)
+
+    to_write = parse_message(msg, exgausters)
+    await write_to_mongo(to_write)
 
 
 async def consume():
@@ -67,22 +104,21 @@ async def consume():
                                 security_protocol=SASL_SSL,
                                 enable_auto_commit=False,
                                 auto_offset_reset=offset,
-
                                 )
+    exgausters_cursor = db.exgauster.find()
+    exgausters = [e for e in await exgausters_cursor.to_list(length=100)]
     await consumer.start()
 
     try:
         async with asyncio.TaskGroup() as tg:
             async for msg in consumer:
-                print(msg.value.decode())
-                break
-                tg.create_task(handle_message(msg.value))
+                print(msg.offset)
+                tg.create_task(handle_message(msg.value, exgausters))
 
     finally:
         await consumer.stop()
 
 
 if __name__ == '__main__':
-    print(signals_mapping)
     loop = asyncio.get_event_loop()
     loop.run_until_complete(consume())
