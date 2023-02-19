acdaeeb9f349c70d4ab5042c6a6c699de00dd4c1
dgolov <dmitriy.evgenyevich@gmail.com>
2023-02-19 09:11:24 +0300

    added cors

--- api/app/app.py
+++ api/app/app.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.db.mongo import db
 from .api_v1 import api_router as api_v1_router
@@ -6,6 +7,21 @@ from .core.config import config
 
 app = FastAPI(
     title=config.PROJECT_NAME,
+)
+
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(api_v1_router, prefix=config.API_V1_URL)
